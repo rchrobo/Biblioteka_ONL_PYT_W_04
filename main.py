007caa01440f1a31a6356ad358fb32a693bf8eb1
cooklee <sbbogus@gmail.com>
2021-02-07 12:23:02 +0100

    rozszeżanie html

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
-from sql_funk import create_author
+from sql_funk import create_author, create_publisher
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html')
 
 
 @app.route("/author/", methods=['GET', 'POST'])
@@ -16,6 +16,14 @@ def author():
         create_author(request.form['first_name'], request.form['last_name'])
         return render_template('author.html')
 
+
+@app.route("/publisher/", methods=['GET', 'POST'])
+def publisher():
+    if request.method == 'GET':
+        return render_template('publisher.html')
+    else:
+        create_publisher(request.form['first_name'], request.form['last_name'])
+        return render_template('publisher.html')
 
 
 if __name__ == '__main__':
--- sql_funk.py
+++ sql_funk.py
@@ -9,6 +9,15 @@ def create_author(first_name, last_name):
     alibaba.execute(sql)
     sikorka.close()
 
+def create_publisher(name, city):
+    sql = f"""
+    INSERT INTO author (name, city) VALUES ('{name}', '{city}');
+    """
+    sikorka = connect()
+    alibaba = sikorka.cursor()
+    alibaba.execute(sql)
+    sikorka.close()
+
 
 if __name__ == "__main__":
     create_author('slawek', 'boguslawski')
